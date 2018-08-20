1dcca1bc54b3185641dcf32abab0b7e9b85d61e5
Zoran Joka <zoran.joka@gmail.com>
2018-08-20 16:15:55 +0200

    implement routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const passport = require('passport')
 const upload = require('./../middleware/multer')
 const swaggerSpec = require('./../../config/initializers/swagger')
@@ -39,8 +40,9 @@ module.exports = (Router, Service, Logger, App) => {
         const id = userData.userId
         const email = userData.data[0].value
         Service.User.FindOrCreate({ id, email })
-          .then((result) => {
-            res.status(201).json(result)
+          .then((user) => {
+            const token = jwt.sign(user.email, App.config.get('secrets').JWT);
+            res.status(201).json({ user, token })
           }).catch((err) => {
             res.send(err.message)
           });
@@ -49,23 +51,138 @@ module.exports = (Router, Service, Logger, App) => {
       });
   });
 
-
-  Router.get('/user/:id', function(req, res) {
+  /**
+   * @swagger
+   * /user/:id:
+   *   post:
+   *     description: Get user.
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: folderId
+   *         description: ID of user
+   *         in: query
+   *         required: true
+   *     responses:
+   *       200:
+  */
+  Router.get('/users/:id', passportAuth, function(req, res) {
     Service.User.GetUserById(req.params.id).then(function(foundUser) {
       res.send(foundUser);
     });
   });
 
-  Router.get('/storage/folder/:id', function(req, res) {
-    // TODO
+  /**
+   * @swagger
+   * /storage/folder/:id:
+   *   post:
+   *     description: Get folder contents.
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: folderId
+   *         description: ID of folder in XCloud
+   *         in: query
+   *         required: true
+   *     responses:
+   *       200:
+   *         description: Array of folder items
+  */
+  Router.get('/storage/folder/:id', passportAuth, function(req, res) {
+    const folderId = req.params.id
+    Service.Folder.GetContent(folderId)
+      .then((result) => {
+        res.status(200).json(result)
+      }).catch((err) => {
+        res.status(500).json(err)
+      });
   });
 
   Router.get('/storage/folder/:id/meta', function(req, res) {
     // TODO
   });
 
-  Router.post('/storage/folder/:id/upload', function(req, res) {
-    // TODO
+  /**
+   * @swagger
+   * /storage/folder:
+   *   post:
+   *     description: Create folder
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: folderId
+   *         description: ID of folder in XCloud
+   *         in: query
+   *         required: true
+   *     responses:
+   *       200:
+   *         description: Array of folder items
+  */
+  Router.post('/storage/folder', passportAuth, async function(req, res) {
+    const folderName = req.body.folderName
+    const parentFolderId = req.body.parentFolderId
+    const user = await Service.User.FindOrCreate({ id: 1, email: 'jokolino@msn.com', userId: '$2a$08$a6GWBl/qdkh/jZXfqb6NKulBajkQFB4vIhlh53Vooril1H2M2nkXm' })
+    Service.Folder.Create(user, folderName, parentFolderId)
+      .then((result) => {
+        res.status(201).json(result)
+      }).catch((err) => {
+        res.status(500).json(err)
+      });
+  })
+  
+  /**
+   * @swagger
+   * /storage/folder:
+   *   post:
+   *     description: Create folder
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: folderId
+   *         description: ID of folder in XCloud
+   *         in: query
+   *         required: true
+   *     responses:
+   *       200:
+   *         description: Array of folder items
+  */
+  Router.delete('/storage/folder/:id', passportAuth, async function(req, res) {
+    const user = await Service.User.FindOrCreate({ id: 1, email: 'jokolino@msn.com', userId: '$2a$08$a6GWBl/qdkh/jZXfqb6NKulBajkQFB4vIhlh53Vooril1H2M2nkXm' })
+    const folderId = req.params.id
+    Service.Folder.Delete(user, folderId)
+      .then((result) => {
+        res.status(204).json(result)
+      }).catch((err) => {
+        res.status(500).json(err)
+      });
+  })
+
+  /**
+   * @swagger
+   * /storage/folder/:id/upload:
+   *   post:
+   *     description: Upload content to folder
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - name: folderId
+   *         description: ID of folder in XCloud
+   *         in: query
+   *         required: true
+   *     responses:
+   *       200:
+   *         description: Uploaded object
+  */
+  Router.post('/storage/folder/:id/upload', passportAuth, upload.single('xfile'), async function(req, res) {
+    const user = await Service.User.FindOrCreate({ id: 1, email: 'jokolino@msn.com', userId: '$2a$08$a6GWBl/qdkh/jZXfqb6NKulBajkQFB4vIhlh53Vooril1H2M2nkXm' })
+    const xfile = req.file
+    const folderId = req.params.id
+    Service.Files.Upload(user, folderId, xfile.originalname, xfile.path)
+      .then((result) => {
+        res.status(201).json(result)
+      }).catch((err) => {
+        res.status(500).json({ message: err })
+      })
   })
 
   Router.get('./storage/file/:id', function(req, res) {
