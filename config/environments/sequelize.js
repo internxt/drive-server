897351ea1d349ab2a05a58021417284003c43b3a
Zoran Joka <zoran.joka@gmail.com>
2018-08-21 13:36:56 +0200

    change sequelize config

---
+++ config/environments/sequelize.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    dialect: 'mysql',
+    database: 'xcloud_server_dev',
+    username: 'developer',
+    password: 'asdf1234',
+    host: 'localhost'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  staging: {
+    host: process.env.RDS_HOSTNAME,
+    database: process.env.RDS_DBNAME,
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    dialect: 'mysql'
+  }
+}
