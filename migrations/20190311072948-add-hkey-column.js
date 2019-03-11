f205475484faf69a567f8a62db6338aad8463db2
JJGonzalez <jesusjuangonzalez@gmail.com>
2019-03-11 08:59:02 +0100

    Added key column to u table

---
+++ migrations/20190311072948-add-hkey-column.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'hKey', 
+    { 
+      type: Sequelize.STRING, 
+      allowNull: false 
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users','hKey');
+  }
+};
