5ec3f549d45d3628d346c6a250b2d9d8f13e2412
JJGonzalez <jesusjuangonzalez@gmail.com>
2019-04-09 15:29:35 +0200

    Added file buckets migrations file

---
+++ migrations/20190405065451-file-buckets-fixes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn('files', 'fileId', {
+        type: Sequelize.STRING
+      }),
+      queryInterface.addColumn('files', 'bucket', {
+        type: Sequelize.STRING
+      })
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.removeColumn('files','fileId'),
+      queryInterface.removeColumn('files','bucket')
+    ]);
+  }
+};
