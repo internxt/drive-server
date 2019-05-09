6944f5bec219a6b985b058513adbb55d23b29392
J. Alberto González <alberto.msn@gmail.com>
2019-05-09 10:10:58 +0200

    new table for shares

---
+++ migrations/20190509094600-create_share_table.js
@@ -0,0 +1,30 @@
+'use strict'
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('shares', {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true
+            },
+            token: {
+                type: Sequelize.STRING
+            },
+            user: {
+                type: Sequelize.STRING
+            },
+            file: {
+                type: Sequelize.STRING
+            },
+            mnemonic: {
+                type: Sequelize.BLOB('medium')
+            }
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('shares');
+    }
+}
