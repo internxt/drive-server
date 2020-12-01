9ff4fd62947bec0126aaec5c658d155174043d6c
demidi <demidietsinf@gmail.com>
2020-12-01 14:28:11 +0100

    send mail invitation team

--- app/services/mail.js
+++ app/services/mail.js
@@ -52,17 +52,7 @@ module.exports = (Model, App) => {
   
 
   const sendEmailTeamsMember = (member, cryptedToken, teamName) => {
-    const mailer = new InternxtMailer({
-        host: process.env.STORJ_MAILER_HOST,
-        port: process.env.STORJ_MAILER_PORT,
-        secure: process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production',
-        auth: {
-          user: process.env.STORJ_MAILER_USERNAME,
-          pass: process.env.STORJ_MAILER_PASSWORD,
-        },
-        from: 'hello@internxt.com',
-    });
-
+    const mailer = mailInstance();
     return new Promise((resolve, reject) => {
       mailer.dispatch(
         member, 
