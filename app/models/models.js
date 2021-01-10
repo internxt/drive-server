310aa1270a2efcae4da0cdb7ff697b4e6754491c
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-01-10 19:55:12 +0000

    UPGRADE sequelize

--- app/models/models.js
+++ app/models/models.js
@@ -1,7 +1,8 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 const fs = require('fs');
 const path = require('path');
-
-const _ = require('lodash');
+const sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 
@@ -10,12 +11,7 @@ module.exports = (Database) => {
   fs.readdirSync(__dirname)
     .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
     .forEach((file) => {
-      const model = Database.instance.import(path.join(__dirname, file));
-
-      if (_.has(model.sequelize.models, 'folderancestor')) {
-        db.folderancestor = model.sequelize.models.folderancestor;
-      }
-
+      const model = require(path.join(__dirname, file))(Database.instance, sequelize.DataTypes);
       db[model.name] = model;
     });
   Object.keys(db).forEach((modelName) => {
