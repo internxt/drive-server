00c3d158023b4cb45810a5f0cc5b4cc1d674a047
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-03-01 09:13:21 +0100

    routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -15,6 +15,7 @@ const MobileRoutes = require('./mobile');
 const TwoFactorRoutes = require('./twofactor');
 const ExtraRoutes = require('./extra');
 const AppSumoRoutes = require('./appsumo');
+const PhotosRoutes = require('./photos');
 
 const passport = require('../middleware/passport');
 const TeamsRoutes = require('./teams');
@@ -45,6 +46,8 @@ module.exports = (Router, Service, App) => {
   TeamsRoutes(Router, Service, App);
   // AppSumo routes
   AppSumoRoutes(Router, Service, App);
+  // Routes used by Internxt Photos
+  PhotosRoutes(Router, Service, App);
 
   /**
    * @swagger
