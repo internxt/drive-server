da5cc90a7e2a008c2855ced1a4876af95f4dc8fd
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-05-24 10:55:26 +0200

    add deletedAt field in files migrations

--- migrations/20210520164800-add-deleted-column-files.js
+++ migrations/20210520164800-add-deleted-column-files.js
@@ -2,11 +2,15 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('files', 'deleted', {
       type: Sequelize.BOOLEAN,
-      defaultValue: false
+      defaultValue: false,
+      allowNull: false
     });
+
+    await queryInterface.addColumn('files', 'deleted_at', { type: Sequelize.DATE });
   },
 
   down: async (queryInterface) => {
     await queryInterface.removeColumn('files', 'deleted');
+    await queryInterface.removeColumn('files', 'deleted_at');
   }
 };
