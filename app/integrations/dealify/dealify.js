acadfb49a0cd56438141302cccd83f4fcfc08ec7
Joan Mora <elcorreudejoanmora@gmail.com>
2021-07-15 16:15:32 +0200

    Computes dealify coupon space

---
+++ app/integrations/dealify/dealify.js
@@ -0,0 +1,19 @@
+const { dealify99, dealify49 } = require('./dealifyProducts');
+
+function couponBytes(coupon) {
+  if (!coupon) {
+    return null;
+  }
+  const dealifyProduct = coupon.slice(-2);
+  let maxSpaceBytes = null;
+  if (dealifyProduct === '49' && dealify49.includes(coupon)) {
+    maxSpaceBytes = 214748364800; // 200 GB
+  } else if (dealifyProduct === '99' && dealify99.includes(coupon)) {
+    maxSpaceBytes = 1099511627776; // 1TB
+  }
+  return maxSpaceBytes;
+}
+
+module.exports = {
+  couponBytes
+};
