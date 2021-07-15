0d9a2baecac9999a22b0828228ff8b723d7a1035
Joan Mora <elcorreudejoanmora@gmail.com>
2021-07-15 16:16:29 +0200

    Constants updated

--- app/services/constants.js
+++ app/services/constants.js
@@ -1,3 +1,6 @@
-module.exports = {
-  FREE_PLAN_BYTES: 10737418240
+module.exports = () => {
+  return {
+    FREE_PLAN_BYTES: 10737418240,
+    SYNC_KEEPALIVE_INTERVAL_MS: 60 * 1000 // 60 seconds
+  };
 };
