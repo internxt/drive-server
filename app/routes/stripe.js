372be4510c50433d49b68a3c323fd3d787144f64
Carlos Sala Samper <carlos15695@hotmail.com>
2021-09-15 17:45:22 +0200

    stripe session v3

--- app/routes/stripe.js
+++ app/routes/stripe.js
@@ -72,8 +72,8 @@ module.exports = (Router, Service) => {
 
         const sessionParams = {
           payment_method_types: ['card'],
-          success_url: req.body.SUCCESS_URL || process.env.HOST_DRIVE_WEB,
-          cancel_url: req.body.CANCELED_URL || process.env.HOST_DRIVE_WEB,
+          success_url: req.body.successUrl || process.env.HOST_DRIVE_WEB,
+          cancel_url: req.body.canceledUrl || process.env.HOST_DRIVE_WEB,
           subscription_data: {
             items: [{ plan: req.body.plan }]
           },
@@ -107,7 +107,9 @@ module.exports = (Router, Service) => {
   Router.post('/stripe/teams/session', passportAuth, async (req, res) => {
     const test = req.body.test || false;
     const stripe = test ? StripeTest : StripeProduction;
-    const { quantity } = req.body;
+    const {
+      mode, successUrl, canceledUrl, priceId, quantity
+    } = req.body;
 
     async.waterfall([
       async () => {
@@ -130,16 +132,15 @@ module.exports = (Router, Service) => {
       async (bridgeUser) => {
         const sessionParams = {
           payment_method_types: ['card'],
-          success_url: `${process.env.HOST_DRIVE_WEB}/team/success/{CHECKOUT_SESSION_ID}`,
-          cancel_url: `${process.env.HOST_DRIVE_WEB}/team/cancel`,
-          subscription_data: {
-            items: [
-              {
-                plan: req.body.plan,
-                quantity
-              }
-            ]
-          },
+          success_url: successUrl || `${process.env.HOST_DRIVE_WEB}/team/success/{CHECKOUT_SESSION_ID}`,
+          cancel_url: canceledUrl || `${process.env.HOST_DRIVE_WEB}/account?tab=plans`,
+          mode,
+          line_items: [
+            {
+              price: priceId,
+              quantity
+            }
+          ],
           metadata: {
             is_teams: true,
             total_members: quantity,
@@ -169,6 +170,92 @@ module.exports = (Router, Service) => {
   });
 
   /**
+   * Should create a new Stripe Session token.
+   * Stripe Session is neccesary to perform a new payment
+   */
+  Router.post('/v2/stripe/session', passportAuth, (req, res) => {
+    const stripe = req.body.test ? StripeTest : StripeProduction;
+    const user = req.user.email;
+
+    async.waterfall([
+      (next) => {
+        // Retrieve the customer by email, to check if already exists
+        stripe.customers.list({
+          limit: 1,
+          email: user
+        }, (err, customers) => {
+          next(err, customers.data[0]);
+        });
+      },
+      (customer, next) => {
+        if (!customer) {
+          // The customer does not exists
+          // Procede to the subscription
+          return next(null, undefined);
+        }
+        // Get subscriptions
+        return stripe.subscriptions.list({
+          customer: customer.id
+        }, (err, response) => {
+          if (err) {
+            return next();
+          }
+          const subscriptions = response.data;
+          if (subscriptions.length === 0) {
+            return next(null, { customer, subscription: null });
+          }
+          const subscription = subscriptions[0];
+          return next(null, { customer, subscription });
+        });
+      },
+      (payload, next) => {
+        if (!payload) {
+          next(null, {});
+        } else {
+          const { customer } = payload;
+
+          next(null, customer);
+        }
+      },
+      (customer, next) => {
+        // Open session
+        const customerId = customer !== null ? customer.id || null : null;
+
+        const sessionParams = {
+          payment_method_types: ['card'],
+          success_url: req.body.SUCCESS_URL || process.env.HOST_DRIVE_WEB,
+          cancel_url: req.body.CANCELED_URL || `${process.env.HOST_DRIVE_WEB}/account?tab=plans`,
+          mode: req.body.mode,
+          line_items: [
+            {
+              price: req.body.priceId,
+              quantity: 1
+            }
+          ],
+          customer_email: user,
+          customer: customerId,
+          allow_promotion_codes: true,
+          billing_address_collection: 'required'
+        };
+
+        if (sessionParams.customer) {
+          delete sessionParams.customer_email;
+        } else {
+          delete sessionParams.customer;
+        }
+
+        stripe.checkout.sessions.create(sessionParams).then((result) => { next(null, result); }).catch((err) => { next(err); });
+      }
+    ], (err, result) => {
+      if (err) {
+        res.status(500).send({ error: err.message });
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  });
+
+  /**
    * Retrieve products listed in STRIPE.
    * Products must be inserted on stripe using the dashboard with the required metadata.
    * Required metadata:
@@ -187,6 +274,16 @@ module.exports = (Router, Service) => {
     const test = req.query.test === 'true' || false;
 
     Service.Stripe.getAllStorageProducts(test).then((products) => {
+      res.status(200).send(products);
+    }).catch((err) => {
+      res.status(500).send({ error: err });
+    });
+  });
+
+  Router.get('/v3/stripe/products', (req, res) => {
+    const test = req.query.test === 'true';
+
+    Service.Stripe.getAllStorageProducts2(test).then((products) => {
       res.status(200).send(products);
     }).catch((err) => {
       res.status(500).send({ error: err });
