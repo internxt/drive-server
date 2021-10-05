d9346d303f5be1f275ee0d0df716f50a20603e0a
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-10-05 08:24:40 +0200

    allowNull to true

--- migrations/20211004060909-add_userid_files.js
+++ migrations/20211004060909-add_userid_files.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('files', 'user_id', {
       type: Sequelize.INTEGER,
-      allowNull: false,
+      allowNull: true,
       references: {
         model: 'users',
         key: 'id'
