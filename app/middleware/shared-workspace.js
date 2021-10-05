e65f13fab1e4ea8d0568bc14cb4419eb8e9fe8a3
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-10-05 14:40:45 +0200

    shared workspace middleware

---
+++ app/middleware/shared-workspace.js
@@ -0,0 +1,19 @@
+const build = (Service) => async (req, res, next) => {
+  try {
+    const { user } = req;
+    const isGuest = user.email !== user.bridgeUser;
+
+    if (!isGuest) {
+      req.behalfUser = req.user;
+    } else {
+      req.behalfUser = await Service.Guest.getHost(user.bridgeUser);
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = {
+  build
+}
