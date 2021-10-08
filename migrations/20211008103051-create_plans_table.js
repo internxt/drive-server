fa66d9a234053669392fba8316179665d15d42ae
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-10-08 12:58:12 +0200

    plans table migration

---
+++ migrations/20211008103051-create_plans_table.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('plans', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.ENUM('subscription', 'one_time')
+      },
+      created_at: {
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        type: Sequelize.DATE
+      },
+      limit: {
+        type: Sequelize.INTEGER
+      }
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('plans');
+  }
+};
