c377699d81ac50a8d2d442aa922bef0c99b0440e
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-10-08 12:58:02 +0200

    plan model

---
+++ app/models/plan.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+  const plan = sequelize.define(
+    'plan',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        reference: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      name: {
+        type: DataTypes.STRING
+      },
+      type: {
+        type: Sequelize.ENUM('subscription', 'one_time')
+      },
+      created_at: {
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        type: Sequelize.DATE
+      },
+      limit: {
+        type: DataTypes.INTEGER
+      }
+    },
+    {
+      timestamps: true,
+      underscored: true
+    }
+  );
+
+  plan.associate = (models) => {
+    plan.belongsTo(models.users);
+  };
+
+  return plan;
+};
