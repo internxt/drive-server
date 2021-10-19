3a212a6812f23188b9f8b84f1d78b81b21e2ecdb
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2021-10-19 09:44:21 +0200

    default constraint name

--- migrations/20210416202800-keyserver_cascade.js
+++ migrations/20210416202800-keyserver_cascade.js
@@ -1,6 +1,10 @@
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.removeConstraint('keyserver', 'keyserver_ibfk_1');
+    // Default constraint name is different in each
+    const foreignKeys = await queryInterface.getForeignKeyReferencesForTable('keyserver');
+    const userIdForeignKey = foreignKeys.find((constraint) => constraint.columnName === 'user_id');
+
+    await queryInterface.removeConstraint('keyserver', userIdForeignKey.constraintName);
     await queryInterface.addConstraint('keyserver', {
       type: 'FOREIGN KEY',
       fields: ['user_id'],
