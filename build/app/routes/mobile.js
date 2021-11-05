d7c2fe70d27fdad3800743bc0f075b968de90e36
Sergio Gutiérrez Villalba <gv.sergio@gmail.com>
2021-11-05 17:52:35 +0100

    remove build from repo

--- build/app/routes/mobile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var bip39 = require('bip39');
-module.exports = function (Router, Service, App) {
-    Router.get('/bits', function (req, res) {
-        var newBits = bip39.generateMnemonic(256);
-        var eNewBits = App.services.Crypt.encryptText(newBits);
-        res.status(200).send({ bits: eNewBits });
-    });
-};
