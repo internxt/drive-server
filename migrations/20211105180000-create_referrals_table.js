2b810ed79939c0e58fb816555b17e5b8088700f5
Sergio Gutiérrez Villalba <gv.sergio@gmail.com>
2021-11-05 18:08:48 +0100

    split referrals and users_referrals migrations

--- migrations/20211105180000-create_referrals_table.js
+++ migrations/20211105180000-create_referrals_table.js
@@ -36,62 +36,9 @@ module.exports = {
         type: Sequelize.DATE
       }
     });
-
-    await queryInterface.createTable('users_referrals', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-      },
-      user_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'users',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        allowNull: false
-      },
-      referral_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'referrals',
-          key: 'id'
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        allowNull: false
-      },
-      referred: {
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      start_date: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      expiration_date: {
-        type: Sequelize.DATE,
-        allowNull: true
-      },
-      applied: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: false
-      },
-      created_at: {
-        type: Sequelize.DATE
-      },
-      updated_at: {
-        type: Sequelize.DATE
-      }
-    });
   },
 
   down: async (queryInterface) => {
-    await queryInterface.dropTable('users_referrals');
     await queryInterface.dropTable('referrals');
   }
 };
