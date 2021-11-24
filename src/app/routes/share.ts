2b137255595d1fe25a0b5c2058f8d6fe2c0cc7d6
sergiogutierrezvillalba <serguvil@upv.edu.es>
2021-11-24 08:00:08 +0100

    share router to ts

---
+++ src/app/routes/share.ts
@@ -0,0 +1,28 @@
+import { Request, Router, Response } from 'express';
+
+import { passportAuth } from '../middleware/passport';
+import sharedMiddlewareBuilder from '../middleware/shared-workspace';
+import { UserAttributes } from '../models/user';
+
+type AuthorizedRequest = Request & { behalfUser: UserAttributes };
+
+class ShareController {
+  private service: any;
+
+  constructor(service: any) {
+    this.service = service;
+  }
+
+  async listShares(req: Request, res: Response) {
+    const list = await this.service.Share.list((req as AuthorizedRequest).behalfUser);
+    
+    res.status(200).send(list);
+  }
+}
+
+export default (router: Router, service: any) => {
+  const controller = new ShareController(service);
+  const sharedAdapter = sharedMiddlewareBuilder.build(service);
+
+  router.get('/share/list', passportAuth, sharedAdapter, controller.listShares.bind(controller));
+};
