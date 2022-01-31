2a7f1d49170fc4b348478608e9286e1ab062553d
Joan Mora <elcorreudejoanmora@gmail.com>
2022-01-31 17:20:38 +0100

    Analytics endpoints

--- src/app/routes/analytics.js
+++ src/app/routes/analytics.js
@@ -1,4 +1,5 @@
 const { passportAuth } = require('../middleware/passport');
+const { page } = require('../middleware/analytics');
 const AnalyticsService = require('../../lib/analytics/AnalyticsService');
 
 module.exports = (Router) => {
@@ -6,5 +7,13 @@ module.exports = (Router) => {
     res.status(200).send();
     const { actionName } = req.body;
     AnalyticsService.actions[actionName](req);
+  });
+
+  Router.post('/data/p', page, (req, res) => {
+    res.status(200).send();
+  });
+
+  Router.post('/data/t', page, (req, res) => {
+    res.status(200).send();
   });
 };
