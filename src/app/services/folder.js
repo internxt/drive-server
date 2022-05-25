3ea0cb047b604526ae55821c83597047544be4bb
Alex Menor <alex.menor@icloud.com>
2022-05-25 10:57:46 +0200

    allow folder names that end with space

--- src/app/services/folder.js
+++ src/app/services/folder.js
@@ -6,7 +6,7 @@ const AesUtil = require('../../lib/AesUtil');
 const logger = require('../../lib/logger').default.getInstance();
 const { default: Redis } = require('../../config/initializers/redis');
 
-const invalidName = /[\\/]|[. ]$/;
+const invalidName = /[\\/]|^\s*$/;
 
 const { Op } = sequelize;
 
@@ -162,10 +162,12 @@ module.exports = (Model, App) => {
 
   const GetTree = async (user, rootFolderId = null) => {
     const rootElements = [];
-    const pendingFolders = [{
-      folderId: rootFolderId || user.root_folder_id,
-      elements: rootElements
-    }];
+    const pendingFolders = [
+      {
+        folderId: rootFolderId || user.root_folder_id,
+        elements: rootElements,
+      },
+    ];
 
     while (pendingFolders.length) {
       const { folderId, elements } = pendingFolders.shift();
@@ -175,10 +177,10 @@ module.exports = (Model, App) => {
 
       const folders = await getChildrenFoldersByFolderId(folderId);
 
-      folders.forEach(f => {
+      folders.forEach((f) => {
         pendingFolders.push({
           folderId: f.id,
-          elements: folder.children
+          elements: folder.children,
         });
       });
 
@@ -193,8 +195,8 @@ module.exports = (Model, App) => {
       raw: true,
       where: {
         id: {
-          [Op.eq]: folderId
-        }
+          [Op.eq]: folderId,
+        },
       },
     });
   };
@@ -204,8 +206,8 @@ module.exports = (Model, App) => {
       raw: true,
       where: {
         parent_id: {
-          [Op.eq]: folderId
-        }
+          [Op.eq]: folderId,
+        },
       },
     });
   };
@@ -215,7 +217,7 @@ module.exports = (Model, App) => {
       raw: true,
       where: {
         folder_id: {
-          [Op.eq]: folderId
+          [Op.eq]: folderId,
         },
       },
     });
@@ -529,9 +531,9 @@ module.exports = (Model, App) => {
   /**
    * Get directory files paginated
    * @param {number} directoryId Folder id
-   * @param {*} offset 
-   * @param {*} limit 
-   * @returns directory files 
+   * @param {*} offset
+   * @param {*} limit
+   * @returns directory files
    */
   const getDirectoryFiles = async (directoryId, offset, limit) => {
     const rawFiles = await Model.file.findAll({
@@ -541,9 +543,7 @@ module.exports = (Model, App) => {
       },
       offset,
       limit,
-      order: [
-        ['id', 'ASC']
-      ]
+      order: [['id', 'ASC']],
     });
 
     const files = [];
@@ -551,7 +551,7 @@ module.exports = (Model, App) => {
     for (const file of rawFiles) {
       files.push({
         ...file,
-        name: App.services.Crypt.decryptName(file.name, file.folder_id)
+        name: App.services.Crypt.decryptName(file.name, file.folder_id),
       });
     }
 
@@ -561,8 +561,8 @@ module.exports = (Model, App) => {
   /**
    * Get directory folders paginated
    * @param {number} directoryId Folder id
-   * @param {*} offset 
-   * @param {*} limit 
+   * @param {*} offset
+   * @param {*} limit
    * @returns directory folders
    */
   const getDirectoryFolders = async (directoryId, offset, limit) => {
@@ -573,9 +573,7 @@ module.exports = (Model, App) => {
       },
       offset,
       limit,
-      order: [
-        ['id', 'ASC']
-      ]
+      order: [['id', 'ASC']],
     });
 
     const folders = [];
@@ -583,7 +581,7 @@ module.exports = (Model, App) => {
     for (const folder of rawFolders) {
       folders.push({
         ...folder,
-        name: App.services.Crypt.decryptName(folder.name, folder.parentId)
+        name: App.services.Crypt.decryptName(folder.name, folder.parentId),
       });
     }
 
@@ -609,6 +607,6 @@ module.exports = (Model, App) => {
     releaseLock,
     refreshLock,
     getDirectoryFiles,
-    getDirectoryFolders
+    getDirectoryFolders,
   };
 };
