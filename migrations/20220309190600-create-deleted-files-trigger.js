74631ee033b02802a48611122e367b319d2c49ac
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-07-20 11:30:31 +0200

    fix(migrations): make migrations postgres-compatible

--- migrations/20220309190600-create-deleted-files-trigger.js
+++ migrations/20220309190600-create-deleted-files-trigger.js
@@ -1,15 +1,28 @@
 module.exports = {
   up: async (queryInterface) => {
+    await queryInterface.sequelize.query(`
+    CREATE OR REPLACE FUNCTION insert_deleted_files()
+      RETURNS trigger AS
+      $$
+      BEGIN
+      INSERT INTO deleted_files(file_id, user_id, folder_id, bucket) select file_id, user_id, folder_id, bucket from files where files.folder_id = OLD.id;
+      RETURN NEW;
+      END;
+      $$
+      LANGUAGE 'plpgsql';`);
     await queryInterface.sequelize.query(
-      `CREATE TRIGGER if not exists xCloud.copy_deleted_files_on_delete BEFORE DELETE on xCloud.folders
+      `DROP TRIGGER IF EXISTS copy_deleted_files_on_delete ON folders;`,
+    );
+    await queryInterface.sequelize.query(
+      `CREATE TRIGGER copy_deleted_files_on_delete BEFORE DELETE on folders
 			FOR EACH ROW
-			BEGIN
-			INSERT INTO xCloud.deleted_files(file_id, user_id, folder_id, bucket) select file_id, user_id, folder_id, bucket from xCloud.files where xCloud.files.folder_id = OLD.id;
-			END`,
+			EXECUTE PROCEDURE insert_deleted_files()`,
     );
   },
 
   down: async (queryInterface) => {
-    await queryInterface.sequelize.query('drop trigger xCloud.copy_deleted_files_on_delete;');
+    await queryInterface.sequelize.query(
+      `DROP TRIGGER IF EXISTS copy_deleted_files_on_delete ON folders;`,
+    );
   },
 };
