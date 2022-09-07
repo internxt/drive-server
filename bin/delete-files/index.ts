5fb26c5c9debe83020d434e14aacaf1f8a0fb6c1
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-09-07 14:42:51 +0200

    fix(delete-files): migrate script to postgres

--- bin/delete-files/index.ts
+++ bin/delete-files/index.ts
@@ -6,63 +6,68 @@ import initDeletedFileModel from '../../src/app/models/deletedFile';
 import { createTimer, deleteFiles, DeleteFilesResponse, getFilesToDelete, signToken } from './utils';
 
 type CommandOptions = {
-  secret: string
-  dbHostname: string
-  dbName: string
-  dbUsername: string
-  dbPassword: string
-  concurrency?: string
-  limit?: string
-  endpoint: string
-}
+  secret: string;
+  dbHostname: string;
+  dbPort: string;
+  dbName: string;
+  dbUsername: string;
+  dbPassword: string;
+  concurrency?: string;
+  limit?: string;
+  endpoint: string;
+};
 
-const commands: { flags: string, description: string, required: boolean }[] = [
+const commands: { flags: string; description: string; required: boolean }[] = [
   {
     flags: '-s, --secret <token_secret>',
     description: 'The secret used to sign the token to request files deletion',
-    required: true
+    required: true,
   },
   {
     flags: '--db-hostname <database_hostname>',
     description: 'The hostname of the database where deleted files are stored',
-    required: true
+    required: true,
   },
   {
     flags: '--db-name <database_name>',
     description: 'The name of the database where deleted files are stored',
-    required: true
+    required: true,
   },
   {
     flags: '--db-username <database_username>',
     description: 'The username authorized to read and delete from the deleted files table',
-    required: true
+    required: true,
   },
   {
     flags: '--db-password <database_password>',
     description: 'The database username password',
-    required: true
+    required: true,
+  },
+  {
+    flags: '--db-port <database_port>',
+    description: 'The database port',
+    required: true,
   },
   {
     flags: '-c, --concurrency <concurrency>',
     description: 'The concurrency level of the requests that will be made',
-    required: false
+    required: false,
   },
   {
     flags: '-l, --limit <limit>',
     description: 'The files limit to handle each time',
-    required: false
+    required: false,
   },
   {
     flags: '-e, --endpoint <endpoint>',
     description: 'The API endpoint where the delete files requests are sent',
-    required: true
-  }
+    required: true,
+  },
 ];
 
-const command = new Command('delete-files')
-  .version('0.0.1');
+const command = new Command('delete-files').version('0.0.1');
 
-commands.forEach(c => {
+commands.forEach((c) => {
   if (c.required) {
     command.requiredOption(c.flags, c.description);
   } else {
@@ -76,11 +81,18 @@ const opts: CommandOptions = command.opts();
 const db = Database.getInstance({
   sequelizeConfig: {
     host: opts.dbHostname,
+    port: opts.dbPort,
     database: opts.dbName,
     username: opts.dbUsername,
     password: opts.dbPassword,
-    dialect: 'mariadb',
-  }
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 });
 
 const timer = createTimer();
@@ -92,25 +104,23 @@ let failedRequests = 0;
 
 const logIntervalId = setInterval(() => {
   console.log(
-    'DELETION RATE: %s/s | FAILURE RATE %s%', 
-    totalFilesRemoved / (timer.end()/1000),
-    (failedRequests / totalRequests) * 100
+    'DELETION RATE: %s/s | FAILURE RATE %s%',
+    totalFilesRemoved / (timer.end() / 1000),
+    (failedRequests / totalRequests) * 100,
   );
 }, 10000);
 
 function finishProgram() {
   clearInterval(logIntervalId);
 
-  console.log(
-    'TOTAL FILES REMOVED %s | DURATION %ss', 
-    totalFilesRemoved, 
-    (timer.end()/1000).toFixed(2)
-  );
-  db.close().then(() => {
-    console.log('DISCONNECTED FROM DB');
-  }).catch((err) => {
-    console.log('Error closing connection %s. %s', err.message. err.stack || 'NO STACK.');
-  });
+  console.log('TOTAL FILES REMOVED %s | DURATION %ss', totalFilesRemoved, (timer.end() / 1000).toFixed(2));
+  db.close()
+    .then(() => {
+      console.log('DISCONNECTED FROM DB');
+    })
+    .catch((err) => {
+      console.log('Error closing connection %s. %s', err.message.err.stack || 'NO STACK.');
+    });
 }
 
 process.on('SIGINT', () => finishProgram());
@@ -123,22 +133,15 @@ async function start(limit = 20, concurrency = 5) {
   do {
     const files = await getFilesToDelete(deletedFile, limit);
 
-    fileIds = files.map(f => f.fileId);
- 
-    const promises = [];
-    const chunksOf = Math.ceil(limit/concurrency);
+    fileIds = files.map((f) => f.fileId);
 
+    const promises = [];
+    const chunksOf = Math.ceil(limit / concurrency);
 
     console.time('df-network-req');
 
     for (let i = 0; i < fileIds.length; i += chunksOf) {
-      promises.push(
-        deleteFiles(
-          opts.endpoint,
-          fileIds.slice(i, i + chunksOf), 
-          signToken('5m', opts.secret)
-        )
-      );
+      promises.push(deleteFiles(opts.endpoint, fileIds.slice(i, i + chunksOf), signToken('5m', opts.secret)));
     }
 
     const results = await Promise.allSettled(promises);
@@ -150,14 +153,14 @@ async function start(limit = 20, concurrency = 5) {
       .flatMap((r) => (r as PromiseFulfilledResult<DeleteFilesResponse>).value.message.confirmed);
 
     totalRequests += results.length;
-    failedRequests += results.filter(r => r.status === 'rejected').length;
+    failedRequests += results.filter((r) => r.status === 'rejected').length;
 
-    const deletedFilesToDelete = files.filter(f => {
-      return filesIdsToRemove.some(fId => fId === f.fileId);
+    const deletedFilesToDelete = files.filter((f) => {
+      return filesIdsToRemove.some((fId) => fId === f.fileId);
     });
 
     if (deletedFilesToDelete.length > 0) {
-      await deletedFile.destroy({ where: { id: { [Op.in]: deletedFilesToDelete.map(f => f.id) }}});
+      await deletedFile.destroy({ where: { id: { [Op.in]: deletedFilesToDelete.map((f) => f.id) } } });
     } else {
       console.warn('Something not going fine, no files deleted');
     }
@@ -169,6 +172,7 @@ async function start(limit = 20, concurrency = 5) {
 start(parseInt(opts.limit || '20'), parseInt(opts.concurrency || '5'))
   .catch((err) => {
     console.log('err', err);
-  }).finally(() => {
+  })
+  .finally(() => {
     finishProgram();
   });
