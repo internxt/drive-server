f949e79b064421b1100eb01627a83763bc1cd239
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2022-09-21 16:25:12 +0200

    refactor(usersReferrals): make webhook work by uuid by default

--- src/app/routes/usersReferrals.js
+++ src/app/routes/usersReferrals.js
@@ -20,38 +20,41 @@ module.exports = (Router, Service) => {
 
   Router.post('/apply-referral/:type?', async (req, res) => {
     const type = req.params.type;
-    let userId, email, key;
-    if(type === 'typeform') {
+    let userId, email, key, uuid;
+    if (type === 'typeform') {
       key = 'complete-survey';
       userId = null;
-      email = req.body.form_response.hidden.email;
-    }else {
+      email = req.body.form_response?.hidden?.email;
+      uuid = req.body.form_response?.hidden?.uuid;
+    } else {
       userId = req.body.userId;
       email = req.body.email;
       key = req.body.key;
     }
-    if(!key) {
-      return res.status(400).send({error: 'Missing referral key'});
+    if (!key) {
+      return res.status(400).send({ error: 'Missing referral key' });
     }
-    if(!userId && email) {
+    if (!userId && uuid) {
+      const user = await Service.User.FindUserByUuid(uuid);
+      userId = user.id;
+    }
+    if (!userId && email) {
       const user = await Service.User.FindUserByEmail(email);
       userId = user.id;
     }
-    if(!userId && !email) {
-      return res.status(400).send({ error: 'UserId or Email are required' });
+    if (!userId && !email) {
+      return res.status(400).send({ error: 'UserId, Email or UUID are required' });
     }
     try {
-      Service.UsersReferrals.applyUserReferral(userId,  key).catch((err) => {
-       logger.error(err);
+      Service.UsersReferrals.applyUserReferral(userId, key).catch((err) => {
+        logger.error(err);
       });
       res.status(200).send();
     } catch (err) {
-      const errMessage = `Error getting user referrals: ${err}`;
+      const errMessage = `Error applying user referral for user with id: ${userId}, error: ${err}`;
 
       logger.error(errMessage);
       res.status(400).send({ error: errMessage });
     }
   });
-
-
 };
