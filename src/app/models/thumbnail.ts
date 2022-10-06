70bed46580e0712d1c0b906c7512ed7527f2e9fe
Larry <larry@internxt.com>
2022-10-06 17:04:31 +0200

    added thumbnails model

---
+++ src/app/models/thumbnail.ts
@@ -0,0 +1,65 @@
+import { Sequelize, ModelDefined, DataTypes } from 'sequelize';
+
+export interface ThumbnailAttributes {
+  id: number;
+  file_id: number;
+  type: string;
+  size: number;
+  bucket_id: string;
+  bucket_file: string;
+  encrypt_version: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export type ThumbnailModel = ModelDefined<ThumbnailAttributes, ThumbnailAttributes>;
+
+export default (database: Sequelize): ThumbnailModel => {
+  const Thumbnail: ThumbnailModel = database.define(
+    'thumbnail',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      file_id: {
+        type: DataTypes.INTEGER
+      },
+      type: {
+        type: DataTypes.STRING
+      },
+      size: {
+        type: DataTypes.BIGINT.UNSIGNED
+      },
+      bucket_id: {
+        type: DataTypes.STRING(24)
+      },
+      bucket_file: {
+        type: DataTypes.STRING(24)
+      },
+      encrypt_version: {
+        type: DataTypes.STRING(20)
+      },
+      created_at: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          return this.getDataValue('created_at');
+        },
+      },
+      updated_at: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          return this.getDataValue('updated_at');
+        },
+      },
+    },
+    {
+      timestamps: true,
+      underscored: true,
+    },
+  );
+
+  return Thumbnail;
+};
