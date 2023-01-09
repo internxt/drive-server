b26918516b2ca76b336a2e4dafc6ed439deb3139
Ramon Candel Segura <ramoncandelsegura@gmail.com>
2023-01-09 09:05:00 +0100

    Added async/await

--- migrations/20230105113000-modify-index-folder-plain-name.js
+++ migrations/20230105113000-modify-index-folder-plain-name.js
@@ -2,18 +2,17 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.removeIndex('folders', 'folders_plainname_parentid_key').then(() =>
-      queryInterface.addIndex('folders', {
-        fields: ['plain_name', 'parent_id'],
-        name: 'folders_plainname_parentid_key',
-        unique: true,
-        where: { deleted: { [Sequelize.Op.eq]: false } },
-      }),
-    );
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeIndex('folders', 'folders_plainname_parentid_key');
+    await queryInterface.addIndex('folders', {
+      fields: ['plain_name', 'parent_id'],
+      name: 'folders_plainname_parentid_key',
+      unique: true,
+      where: { deleted: { [Sequelize.Op.eq]: false } },
+    });
   },
 
-  down: (queryInterface) => {
-    return queryInterface.removeIndex('folders', 'folders_plainname_parentid_key');
+  down: async (queryInterface) => {
+    await queryInterface.removeIndex('folders', 'folders_plainname_parentid_key');
   },
 };
