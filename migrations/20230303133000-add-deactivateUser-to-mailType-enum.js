ee96e75d9a05363c5d5aa030853ca4f15b2ec0dd
larry-internxt <larry@internxt.com>
2023-03-15 11:47:56 +0100

    added sequelize down to deactivateUser mailType migration

--- migrations/20230303133000-add-deactivateUser-to-mailType-enum.js
+++ migrations/20230303133000-add-deactivateUser-to-mailType-enum.js
@@ -4,4 +4,14 @@ module.exports = {
       `ALTER TYPE mail_type ADD VALUE 'deactivate_user';`,
     );
   },
+  down: async (queryInterface) => {
+    await queryInterface.sequelize.query(
+      `
+        ALTER TYPE mail_type RENAME TO mail_type_old;
+        CREATE TYPE mail_type AS ENUM('invite_friend', 'reset_password', 'remove_account');
+        ALTER TABLE mail_limits ALTER COLUMN mail_type TYPE mail_type USING mail_type::text::mail_type;
+        DROP TYPE mail_type_old;
+      `,
+    );
+  },
 };
