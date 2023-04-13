f8bbfa64086f80662a1ae2aa84115a9a83c75815
joan vicens <jwcervera@hotmail.com>
2023-04-13 16:46:41 +0200

    fix: allow file size to be 0 on create file controller

--- src/app/routes/storage.ts
+++ src/app/routes/storage.ts
@@ -49,7 +49,7 @@ export class StorageController {
       file.fileId = file.file_id;
     }
 
-    if (!file || !file.fileId || !file.bucket || !file.size || !file.folder_id || !file.name) {
+    if (!file || !file.fileId || !file.bucket || file.size === undefined || file.size === null || !file.folder_id || !file.name) {
       this.logger.error(
         `Invalid metadata trying to create a file for user ${behalfUser.email}: ${JSON.stringify(file, null, 2)}`,
       );
